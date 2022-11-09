69b5dd3471b343043ce01d78ab624ced16d1e285
jbloom-md <joseph@massdynamics.com>
2022-11-09 18:13:15 +0000

    add parallel loading to reverse sequence task

--- w1d2/train_reverse_sequence_task.py
+++ w1d2/train_reverse_sequence_task.py
@@ -21,55 +21,55 @@ config =  {
     'dropout': 0.1,
     'layer_norm_epsilon': 1e-5,
     'train_set_size': 10**5,
-    'test_set_size': 10**3
+    'test_set_size': 10**3,
+    'num_workers': cpu_count() - 1
 }
 
-wandb.init(
-    project="W1D1 Transformer Tilman and Joseph",
-    name = "second try",
-    entity="arena-ldn",
-    config = config
-    )
+if __name__ == '__main__':
+
+    wandb.init(
+        project="W1D1 Transformer Tilman and Joseph",
+        name = "second try",
+        entity="arena-ldn",
+        config = config
+        )
 
 
+    batch_size = wandb.config.batch_size
+    seq_len = wandb.config.seq_len
+    transformer_config = TransformerConfig(num_layers=wandb.config.num_layers,
+                                        num_heads=wandb.config.num_heads,
+                                        vocab_size=wandb.config.vocab_size,
+                                        hidden_size=wandb.config.hidden_size,
+                                        max_seq_len=wandb.config.seq_len,
+                                        dropout=wandb.config.dropout,
+                                        layer_norm_epsilon=wandb.config.layer_norm_epsilon)
 
+    model = DecoderOnlyTransformer(config=transformer_config)
 
+    from torchinfo import torchinfo
 
-batch_size = 1024
-seq_len = 6
-transformer_config = TransformerConfig(num_layers=wandb.config.num_layers,
-                                       num_heads=wandb.config.num_heads,
-                                       vocab_size=wandb.config.vocab_size,
-                                       hidden_size=wandb.config.hidden_size,
-                                       max_seq_len=wandb.config.seq_len,
-                                       dropout=wandb.config.dropout,
-                                       layer_norm_epsilon=wandb.config.layer_norm_epsilon)
+    torchinfo.summary(model, input_data=t.tensor([[1, 2, 3, 4, 5, 6]]))
 
-model = DecoderOnlyTransformer(config=transformer_config)
+    # get data
+    train_set_size = wandb.config.train_set_size
+    test_set_size = wandb.config.test_set_size
+    num_workers = 0#cpu_count()
 
-from torchinfo import torchinfo
+    train_dataset = RevSequenceDataset(seq_length = seq_len, data_set_size=train_set_size)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    test_dataset = RevSequenceDataset(seq_length = seq_len, data_set_size=test_set_size)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
-torchinfo.summary(model, input_data=t.tensor([[1, 2, 3, 4, 5, 6]]))
-
-# get data
-train_set_size = wandb.config.train_set_size
-test_set_size = wandb.config.test_set_size
-num_workers = 0#cpu_count()
-
-train_dataset = RevSequenceDataset(seq_length = seq_len, data_set_size=train_set_size)
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-test_dataset = RevSequenceDataset(seq_length = seq_len, data_set_size=test_set_size)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-
-# run model
-num_epochs = wandb.config.num_epochs
-device = "cpu" if t.backends.mps.is_available() else "cpu"
-criterion = nn.CrossEntropyLoss()
-optimizer = t.optim.Adam(model.parameters(), lr=wandb.config.lr)
-model = train(model,
-              optimizer,
-              train_loader,
-              test_loader,
-              criterion,
-              num_epochs=num_epochs,
-              device=device)
+    # run model
+    num_epochs = wandb.config.num_epochs
+    device = "cpu" if t.backends.mps.is_available() else "cpu"
+    criterion = nn.CrossEntropyLoss()
+    optimizer = t.optim.Adam(model.parameters(), lr=wandb.config.lr)
+    model = train(model,
+                optimizer,
+                train_loader,
+                test_loader,
+                criterion,
+                num_epochs=num_epochs,
+                device=device)
