eeb4a7c0b38009908d8bcf4faf37372dd1bf412d
Joseph Bloom <joseph.i.bloom@gmail.com>
2022-11-17 16:17:52 +0000

    update envs for working on ubuntu too

---
+++ scripts/env.sh
@@ -0,0 +1,9 @@
+# conda
+
+curl -sL \
+  "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh" > \
+  "Miniconda3.sh"
+bash Miniconda3.sh
+
+python3 -m pip install --upgrade pip
+conda env create -f environment_ubuntu.yml
